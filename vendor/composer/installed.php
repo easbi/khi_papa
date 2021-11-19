d713a5dda7b0d84063c5d24e6acde263f38ebb1d
easterikhxen <easterikhxen@gmail.com>
2021-11-19 13:04:27 +0700

    livewire update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '34e5da2fb51cd7593c23e6b99636ad77d37d61d2',
+        'reference' => '9ca255340fdbbeb38fcc75ffa7a4c5a4bd655549',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -463,7 +463,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '34e5da2fb51cd7593c23e6b99636ad77d37d61d2',
+            'reference' => '9ca255340fdbbeb38fcc75ffa7a4c5a4bd655549',
             'dev_requirement' => false,
         ),
         'laravel/sail' => array(
@@ -868,8 +868,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
