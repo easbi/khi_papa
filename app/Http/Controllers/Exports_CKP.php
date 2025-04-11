b7ae648e7ed5850bce6a3a8ddc88dfa62b4ae169
easbi <easterikhxen@gmail.com>
2025-04-11 14:11:15 +0700

    Version 2.1.1 - Update CKP-T Mengikuti Pengisian KHI Bulan Selanjutnya

--- app/Http/Controllers/Exports_CKP.php
+++ app/Http/Controllers/Exports_CKP.php
@@ -16,14 +16,6 @@ class Exports_CKP extends Controller
 {
     public function exportToExcel($tahun, $bulan)
     {
-        // $activities = DB::table('daily_activity')
-        //     ->whereYear('tgl', '=', date($tahun))
-        //     ->whereMonth('tgl', '=', date($bulan))
-        //     ->where('daily_activity.nip', auth()->user()->nip)
-        //     ->join('users', 'daily_activity.nip', 'users.nip')
-        //     ->select('daily_activity.*', 'users.fullname')
-        //     ->orderBy('id', 'asc')->get();
-
         $mainActivities = DB::table('daily_activity')
             ->whereYear('tgl', '=', $tahun) // Filter by year
             ->whereMonth('tgl', '=', $bulan) // Filter by month
@@ -65,13 +57,13 @@ class Exports_CKP extends Controller
         // Populate data starting from a specific row (e.g., row 2)
         $dummydate = $tahun . '-' . $bulan . '-1'; // Example date
         $row = 13;
+
         $sheet->setCellValue('C' . 5, ":  ". auth()->user()->fullname);
         $sheet->setCellValue('C' . 6, ":  ". auth()->user()->jabatan);
         $sheet->setCellValue('C' . 7, ":  1-". carbon::parse($dummydate)->endOfMonth()->translatedFormat('j F Y'));
         $sheet->setCellValue('C' . 25, auth()->user()->fullname);
         $sheet->setCellValue('C' . 26, "NIP. ". auth()->user()->nip);
         $sheet->setCellValue('B' . 21, "Tanggal : ". Carbon::now()->translatedFormat('j F Y'));
-        // $sheet->setCellValue('C' . 6, ":  ". auth()->user()->);
 
         foreach ($mainActivities as $activity) {
             $sheet->setCellValue('A' . $row, $row-12);
@@ -112,9 +104,39 @@ class Exports_CKP extends Controller
 
         $sheet->removeRow($row);
 
+        
         $sheetName = 'CKP_T';  // Replace 'SheetName' with the actual name of the sheet
         $spreadsheet->setActiveSheetIndexByName($sheetName);
         $sheet = $spreadsheet->getActiveSheet();
+
+        $nextMonth = Carbon::create($tahun, $bulan)->addMonth();
+        $tahunNext = $nextMonth->year;
+        $bulanNext = $nextMonth->month;
+
+        $mainActivitiesNext = DB::table('daily_activity')
+            ->whereYear('tgl', '=', $tahun)
+            ->whereMonth('tgl', '=', $bulanNext)
+            ->where('daily_activity.nip', auth()->user()->nip)
+            ->where('daily_activity.jenis_kegiatan', 'UTAMA')
+            ->where('daily_activity.wfo_wfh', '!=', 'Lainnya')
+            ->join('users', 'daily_activity.nip', '=', 'users.nip')
+            ->select('daily_activity.kegiatan', 'daily_activity.satuan', 'users.fullname', DB::raw('SUM(daily_activity.kuantitas) as total_kuantitas'), DB::raw('MIN(daily_activity.id) as min_id'))
+            ->groupBy('daily_activity.kegiatan', 'daily_activity.satuan', 'users.fullname')
+            ->orderBy('min_id', 'asc')
+            ->get();
+
+        $addActivitiesNext = DB::table('daily_activity')
+            ->whereYear('tgl', '=', $tahun)
+            ->whereMonth('tgl', '=', $bulanNext)
+            ->where('daily_activity.nip', auth()->user()->nip)
+            ->where('daily_activity.jenis_kegiatan', 'TAMBAHAN')
+            ->where('daily_activity.wfo_wfh', '!=', 'Lainnya')
+            ->join('users', 'daily_activity.nip', '=', 'users.nip')
+            ->select('daily_activity.kegiatan', 'daily_activity.satuan', 'users.fullname', DB::raw('SUM(daily_activity.kuantitas) as total_kuantitas'), DB::raw('MIN(daily_activity.id) as min_id'))
+            ->groupBy('daily_activity.kegiatan', 'daily_activity.satuan', 'users.fullname')
+            ->orderBy('min_id', 'asc')
+            ->get();
+
 
         #Ubah Header CKP-T Menjadi Dinamis
         $sheet->setCellValue('A2', 'TARGET KINERJA PEGAWAI TAHUN ' . $tahun);
@@ -122,22 +144,46 @@ class Exports_CKP extends Controller
         $sheet->getStyle('A2')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
         $sheet->getStyle('A2')->getFont()->setName('Quattrocento Sans')->setBold(true)->setSize(14);
 
-        // Populate data starting from a specific row (e.g., row 2)
-        // $dummydate = $tahun . '-' . $bulan+1 . '-1'; // Example date
+        // Populate data starting from a specific row 
+        $row = 13;
 
         // Create a DateTime object from the dummy date
         $newDate = new \DateTime($dummydate);
 
         // Modify the date to move to the next month
         $newDate->modify('+1 month');
-
-        $row = 13;
+        
         $sheet->setCellValue('C' . 5, ":  ". auth()->user()->fullname);
         $sheet->setCellValue('C' . 6, ":  ". auth()->user()->jabatan);
         $sheet->setCellValue('C' . 7, ":  1-". carbon::parse($newDate)->endOfMonth()->translatedFormat('j F Y'));
         $sheet->setCellValue('C' . 23, auth()->user()->fullname);
         $sheet->setCellValue('C' . 24, "NIP. ". auth()->user()->nip);
         $sheet->setCellValue('B' . 19, "Tanggal : ". Carbon::now()->translatedFormat('j F Y'));
+        foreach ($mainActivitiesNext as $activity) {
+            $sheet->setCellValue('A' . $row, $row-12);
+            $sheet->setCellValue('B' . $row, $activity->kegiatan);
+            $sheet->setCellValue('D' . $row, $activity->satuan);
+            $sheet->setCellValue('E' . $row, $activity->total_kuantitas);
+            $row++;
+            $sheet->insertNewRowBefore($row);
+            $sheet->mergeCells('B' . $row . ':C' . $row);
+        }
+        $sheet->removeRow($row);
+
+        $row = $row+1;
+        $no_dummy = $row-1;
+
+        foreach ($addActivitiesNext as $activity) {
+            $sheet->setCellValue('A' . $row, $row-$no_dummy);
+            $sheet->setCellValue('B' . $row, $activity->kegiatan);
+            $sheet->setCellValue('D' . $row, $activity->satuan);
+            $sheet->setCellValue('E' . $row, $activity->total_kuantitas);
+            $row++;
+            $sheet->insertNewRowBefore($row);
+            $sheet->mergeCells('B' . $row . ':C' . $row);
+        }
+        $sheet->removeRow($row);
+
 
         // Save the new Excel file
         $fileName = $tahun . sprintf('%02d', $bulan) . '_CKP_' . auth()->user()->nip . '.xlsx';
